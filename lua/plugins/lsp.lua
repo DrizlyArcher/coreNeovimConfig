3bdf7e94ae3024b5c064b4d662b59df393fac928
CharlieInshaw <charles.inshaw@ovo.com>
2025-07-16 16:13:11 +0100

    lsp keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,6 +24,15 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Show diagnostics" })
+			vim.keymap.set("n", "<leader>gsd", function()
+				vim.cmd("split")
+				vim.lsp.buf.definition()
+			end, { desc = "[G]o to [D]efinition in a horizontal split" })
+			vim.keymap.set("n", "<leader>gvd", function()
+				vim.cmd("vertical split")
+				vim.lsp.buf.definition()
+			end, { desc = "[G]o to [D]efinition in a vertical split" })
 		end,
 	},
 }
