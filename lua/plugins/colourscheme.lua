546327e4418f0bd5a5873da61a6a9b6f2ca512c1
CharlieInshaw <charles.inshaw@ovo.com>
2025-07-21 09:21:40 +0100

    theme toggle, extra themes, gitsign blame always on

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -1,11 +1,46 @@
+-- original tokyonight-strom
+--return {
+--	{
+--		"folke/tokyonight.nvim",
+--		lazy = false,
+--		priority = 1000,
+--		opts = {},
+--		config = function()
+--			vim.cmd([[colorscheme tokyonight-storm]])
+--		end,
+--	},
+--}
+
+-- 2nd scheme, carbonfox
+
+local colourshemes = { "tokyonight-storm", "carbonfox", "everforest" }
+local current_index = 1
+
 return {
 	{
-		"folke/tokyonight.nvim",
+		"EdenEast/nightfox.nvim",
 		lazy = false,
 		priority = 1000,
 		opts = {},
+	},
+	{ "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
+	{
+		"neanias/everforest-nvim",
+		lazy = false,
+		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme tokyonight-storm]])
+			require("everforest").setup({})
 		end,
 	},
+
+	vim.keymap.set("n", "<leader>tc", function()
+		current_index = current_index % #colourshemes + 1
+		local scheme = colourshemes[current_index]
+		vim.cmd("colorscheme " .. scheme)
+		print("Cycles to colorscheme: " .. scheme)
+	end, { desc = "Cycled colorscheme" }),
+
+	-- config = function()
+	--   vim.cmd([[colorscheme carbonfox]])
+	-- end,
 }
