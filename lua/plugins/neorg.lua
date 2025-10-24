478f0ac05f91fc5c3c51af8f79685ca34865160b
CharlieInshaw <charles.inshaw@ovo.com>
2025-10-24 14:42:46 +0100

    neorg-have-a-go
    - Seems to work well
    - Only annoying part is having to create workspaces within the .lua
    config before I can save a location.
     - Creating workspaces on the fly would be really good.

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,32 @@
+local keymap = vim.api.nvim_set_keymap
+
+return {
+  "nvim-neorg/neorg",
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  lazy = false,  -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+  version = "*", -- Pin Neorg to the latest stable release
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              notes = "~./notes",
+              -- Work specific note locations
+              ["cards-notes"] = "~./Desktop/Repositories/rise-pol/apps/cards/charlie-notes",
+              ["rec-cards-notes"] = "~./Desktop/Repositories/rise-pol/apps/reconciliation-cards/charlie-notes"
+            },
+            --default_workspace =  "~./notes"
+          }
+        }
+      },
+    })
+  end,
+  run = ":Neorg sync-parsers",
+
+  -- This first keybing most likely will not work, they do not expose workspaces outside the terminal
+  --vim.keymap.set({ "n", "<leader>nw", ,{buffer = true, desc = "[N]eorg choose [W]orkspace"} }),
+  keymap("n", "<leader>ni", ":Neorg index<CR>", { desc = "[N]eorg open [I]ndex file" })
+}
